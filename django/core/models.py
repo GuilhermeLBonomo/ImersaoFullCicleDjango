6489feb2f18445dcb946ef72e01cce177df8c0b6
GuilhermeLBonomo <Darkshi5044@proton.me>
2024-10-25 17:50:18 -0300

    Commit: 2024-10-25

--- django/core/models.py
+++ django/core/models.py
@@ -3,29 +3,74 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 import hashlib
 import time
-import os
+import re
+from datetime import datetime
 
 
-def random_filename(instance, filename):
+# Função para gerar um nome de arquivo aleatório
+def random_filename(instance, filename: str) -> str:
     # Extrai a extensão do arquivo
     ext = filename.split(".")[-1]
-
     # Usa o timestamp atual e o nome original para gerar o hash simples
     hash_object = hashlib.md5(f"{filename}{time.time()}".encode("utf-8"))
     return f"{hash_object.hexdigest()}.{ext}"
 
 
+def validate_no_special_characters(value: str):
+    print(f"Argumento: {value}")
+    # Permitir letras, números, espaços, acentos, e algumas pontuações
+    # Adicionei essa função pois um dos testes falhou
+    if re.search(r"[<>;]", value):
+        print(f"Argumento inválido: {value}")
+        raise ValidationError(
+            "O campo não deve conter caracteres especiais como '<', '>', ou ';'."
+        )
+    if not re.match(
+        r"^[\w\s!\"#$%&'()*+,-./:;<=>?@[\\\]^\_`{|}~áéíóúãâêîôûäëïöü]+$", value
+    ):
+        print(f"Argumento inválido: {value}")
+        raise ValidationError(
+            "O campo só pode conter letras, números, espaços e pontuação comum."
+        )
+
+
+def validate_file_path(value: str):
+    print(f"Argumento: {value}")
+    if not re.match(r"^[\w\-/\.]+$", value):
+        print(f"Argumento inválido: {value}")
+        raise ValidationError(
+            "O campo vídeo deve conter apenas letras, números, underscores, hífens, barras e pontos."
+        )
+
+
 # Create your models here.
 class Video(models.Model):
-    title = models.CharField(max_length=100, unique=True, verbose_name='Título')
-    description = models.TextField(verbose_name='Descrição')
-    thumbnail = models.ImageField(upload_to=random_filename, verbose_name="Thumbnail")
-    slug = models.SlugField(unique=True)
-    published_at = models.DateTimeField(verbose_name='Publicado em', null=True, editable=False)
-    is_published = models.BooleanField(default=False, verbose_name='Publicado')
-    num_likes = models.IntegerField(default=0, verbose_name='Likes', editable=False)
-    num_views = models.IntegerField(default=0, verbose_name='Visualizações', editable=False)
-    tags = models.ManyToManyField('Tag', verbose_name='Tags', related_name='videos')
+    title: str = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Título",
+        validators=[validate_no_special_characters],
+    )
+    description: str = models.TextField(
+        verbose_name="Descrição", validators=[validate_no_special_characters]
+    )
+    thumbnail: str = models.ImageField(
+        upload_to=random_filename, verbose_name="Thumbnail"
+    )
+    slug: str = models.SlugField(
+        unique=True, validators=[validate_no_special_characters]
+    )
+    published_at: datetime = models.DateTimeField(
+        verbose_name="Publicado em", null=True, editable=False
+    )
+    is_published: bool = models.BooleanField(default=False, verbose_name="Publicado")
+    num_likes: int = models.IntegerField(
+        default=0, verbose_name="Likes", editable=False
+    )
+    num_views: int = models.IntegerField(
+        default=0, verbose_name="Visualizações", editable=False
+    )
+    tags = models.ManyToManyField("Tag", verbose_name="Tags", related_name="videos")
     author = models.ForeignKey(
         "auth.User",
         on_delete=models.PROTECT,
@@ -53,13 +98,13 @@ class Video(models.Model):
                 raise ValidationError("O vídeo não foi processado.")
 
     def get_video_status_display(self):
-        if not hasattr(self, 'video_media'):
-            return 'Pendente'
+        if not hasattr(self, "video_media"):
+            return "Pendente"
         return self.video_media.get_status_display()
 
     class Meta:
-        verbose_name = 'Vídeo'
-        verbose_name_plural = 'Vídeos'
+        verbose_name: str = "Vídeo"
+        verbose_name_plural: str = "Vídeos"
 
     def __str__(self):
         return self.title
@@ -68,29 +113,48 @@ class Video(models.Model):
 class VideoMedia(models.Model):
 
     class Status(models.TextChoices):
-        UPLOADED_STARTED = 'UPLOADED_STARTED', 'Upload Iniciado'
-        PROCESS_STARTED = 'PROCESSING_STARTED', 'Processamento Iniciado'
-        PROCESS_FINISHED = 'PROCESSING_FINISHED', 'Processamento Finalizado'
-        PROCESS_ERROR = 'PROCESSING_ERROR', 'Erro no Processamento'
+        UPLOADED_STARTED = "UPLOADED_STARTED", "Upload Iniciado"
+        PROCESS_STARTED = "PROCESSING_STARTED", "Processamento Iniciado"
+        PROCESS_FINISHED = "PROCESSING_FINISHED", "Processamento Finalizado"
+        PROCESS_ERROR = "PROCESSING_ERROR", "Erro no Processamento"
 
-    video_path = models.CharField(max_length=255, verbose_name='Vídeo')
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.UPLOADED_STARTED, verbose_name='Status')
-    video = models.OneToOneField('Video', on_delete=models.PROTECT, verbose_name='Vídeo', related_name='video_media')
+    video_path: str = models.CharField(
+        max_length=255,
+        verbose_name="Vídeo",
+        validators=[validate_file_path],
+    )
+    status: str = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.UPLOADED_STARTED,
+        verbose_name="Status",
+    )
+    video = models.OneToOneField(
+        "Video",
+        on_delete=models.PROTECT,
+        verbose_name="Vídeo",
+        related_name="video_media",
+    )
 
     def get_status_display(self):
         return VideoMedia.Status(self.status).label
 
     class Media:
-        verbose_name = 'Mídia'
-        verbose_name_plural = 'Mídias'
+        verbose_name: str = "Mídia"
+        verbose_name_plural: str = "Mídias"
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name='Nome')
+    name: str = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name="Nome",
+        validators=[validate_no_special_characters],
+    )
 
     class Meta:
-        verbose_name = 'Tag'
-        verbose_name_plural = 'Tags'
-    
+        verbose_name: str = "Tag"
+        verbose_name_plural: str = "Tags"
+
     def __str__(self):
         return self.name
--- django/core/tests.py
+++ django/core/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 from django.db import connection
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 from docker import from_env
 from psycopg2 import connect, Error
+from django.db.utils import IntegrityError
 from django.conf import settings
 from os import getenv
+from django.urls import reverse
 from re import compile, IGNORECASE
+from .models import Tag, Video, VideoMedia
 
 
 class EnvVariablesTest(TestCase):
@@ -29,7 +34,7 @@ class DockerPostgresTest(TestCase):
         pattern = compile(r"(postgres|\.?django-db-1)", IGNORECASE)
 
         for container in self.client.containers.list():
-            #print(container.name)
+            # print(container.name)
             if pattern.search(container.name):
                 postgres_container = container
                 break
@@ -44,7 +49,7 @@ class DockerPostgresTest(TestCase):
         pattern = compile(r"(pgadmin|\.?django-pgadmin-1)", IGNORECASE)
 
         for container in self.client.containers.list():
-            #print(container.name)
+            # print(container.name)
             if pattern.search(container.name):
                 pgadmin_container = container
                 break
@@ -83,3 +88,203 @@ class PostgresAuthTest(TestCase):
 
         except Exception as e:
             self.fail(f"Database connection failed, unknown error: {str(e)}")
+
+
+class TagCRUDTest(TestCase):
+    def setUp(self):
+        self.tag = Tag.objects.create(name="Matemática")
+
+    def test_create_tag(self):
+        TAMANHO = Tag.objects.all().count()
+        tag = Tag.objects.create(name="Tecnologia da Informação")
+        self.assertEqual(tag.name, "Tecnologia da Informação")
+        self.assertEqual(Tag.objects.all().count(), TAMANHO + 1)
+
+    def test_read_tag(self):
+        tag = Tag.objects.get(name="Matemática")
+        self.assertEqual(tag.name, "Matemática")
+
+    def test_update_tag(self):
+        TAMANHO = Tag.objects.all().count()
+        self.tag.name = "Ciência"
+        self.tag.save()
+        updated_tag = Tag.objects.get(id=self.tag.id)
+        self.assertEqual(updated_tag.name, "Ciência")
+        self.assertNotEqual(updated_tag.name, "Matemática")
+        self.assertEqual(Tag.objects.all().count(), TAMANHO)
+
+    def test_delete_tag(self):
+        TAMANHO = Tag.objects.all().count()
+        self.tag.delete()
+        with self.assertRaises(Tag.DoesNotExist):
+            Tag.objects.get(id=self.tag.id)
+        self.assertEqual(Tag.objects.all().count(), TAMANHO - 1)
+
+    def tearDown(self):
+        Tag.objects.all().delete()
+
+
+class VideoCRUDTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="Bob", password="SenhaTesteBob123$"
+        )
+        self.video = Video.objects.create(
+            title="Aprendizado de Máquina",
+            description="Introdução ao aprendizado de máquina.",
+            slug=slugify("Aprendizado de Máquina"),
+            author=self.user,
+        )
+
+    def test_create_video(self):
+        TAMANHO = Video.objects.all().count()
+        video = Video.objects.create(
+            title="Deep Learning",
+            description="Uma introdução ao deep learning.",
+            slug=slugify("Deep Learning"),
+            author=self.user,
+        )
+        self.assertEqual(video.title, "Deep Learning")
+        self.assertEqual(Video.objects.all().count(), TAMANHO + 1)
+
+    def test_read_video(self):
+        video = Video.objects.get(title="Aprendizado de Máquina")
+        self.assertEqual(video.description, "Introdução ao aprendizado de máquina.")
+
+    def test_update_video(self):
+        TAMANHO = Video.objects.all().count()
+        self.video.title = "Machine Learning Avançado"
+        self.video.save()
+        updated_video = Video.objects.get(id=self.video.id)
+        self.assertEqual(updated_video.title, "Machine Learning Avançado")
+        self.assertNotEqual(updated_video.title, "Aprendizado de Máquina")
+        self.assertEqual(Video.objects.all().count(), TAMANHO)
+
+    def test_delete_video(self):
+        TAMANHO = Video.objects.all().count()
+        self.video.delete()
+        with self.assertRaises(Video.DoesNotExist):
+            Video.objects.get(id=self.video.id)
+        self.assertEqual(Video.objects.all().count(), TAMANHO - 1)
+
+    def tearDown(self):
+        Video.objects.all().delete()
+        User.objects.all().delete()
+
+
+class VideoMediaCRUDTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="autor", password="12345")
+        self.video = Video.objects.create(
+            title="Data Science",
+            description="Introdução à ciência de dados.",
+            slug=slugify("Data Science"),
+            author=self.user,
+        )
+        self.video_media = VideoMedia.objects.create(
+            video_path=r"videos/datascience.mp4",
+            status=VideoMedia.Status.UPLOADED_STARTED,
+            video=self.video,
+        )
+
+    def test_create_video_media(self):
+        TAMANHO = VideoMedia.objects.all().count()
+        new_video = Video.objects.create(
+            title="Big Data",
+            description="Análise de grandes volumes de dados.",
+            slug=slugify("Big Data"),
+            author=self.user,
+        )
+        video_media = VideoMedia.objects.create(
+            video_path=r"videos/bigdata.mp4",
+            status=VideoMedia.Status.PROCESS_FINISHED,
+            video=new_video,
+        )
+        self.assertEqual(video_media.status, VideoMedia.Status.PROCESS_FINISHED)
+        self.assertEqual(VideoMedia.objects.all().count(), TAMANHO + 1)
+
+    def test_read_video_media(self):
+        video_media = VideoMedia.objects.get(video=self.video)
+        self.assertEqual(video_media.video_path, r"videos/datascience.mp4")
+
+    def test_update_video_media(self):
+        TAMANHO = VideoMedia.objects.all().count()
+        self.video_media.status = VideoMedia.Status.PROCESS_FINISHED
+        self.video_media.save()
+        updated_media = VideoMedia.objects.get(id=self.video_media.id)
+        self.assertEqual(updated_media.status, VideoMedia.Status.PROCESS_FINISHED)
+        self.assertNotEqual(updated_media.status, VideoMedia.Status.UPLOADED_STARTED)
+        self.assertEqual(VideoMedia.objects.all().count(), TAMANHO)
+
+    def test_delete_video_media(self):
+        TAMANHO = VideoMedia.objects.all().count()
+        self.video_media.delete()
+        with self.assertRaises(VideoMedia.DoesNotExist):
+            VideoMedia.objects.get(id=self.video_media.id)
+        self.assertEqual(VideoMedia.objects.all().count(), TAMANHO - 1)
+
+    def tearDown(self):
+        VideoMedia.objects.all().delete()
+        Video.objects.all().delete()
+        User.objects.all().delete()
+
+
+class SQLInjectionTest1(TestCase):
+
+    def test_sql_injection_in_tag_name(self):
+        malicious_input = r"Matemática'; DROP TABLE Tag; --"
+
+        try:
+            Tag.objects.create(name=malicious_input)
+            self.fail(
+                "Tag was created despite SQL injection attempt, indicating a vulnerability."
+            )
+            tag_injected = Tag.objects.get(name=malicious_input)
+            self.assertEqual(tag_injected.name, malicious_input)
+        except IntegrityError:
+            #  A criação da tag deve falhar, o que é esperado
+            pass
+        except Exception as e:
+            self.assertNotIn(
+                "DROP TABLE", str(e), "SQL Injection vulnerability detected"
+            )
+        self.assertTrue(
+            Tag.objects.filter(name=malicious_input).exists(),
+            f"The original tag '{malicious_input}' does not exist after SQL injection attempt.",
+        )
+
+    def tearDown(self):
+        # Caso ele crie mas não drope a tabela
+        Tag.objects.all().delete()
+
+
+class SQLInjectionTest2(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="TestUser", password="testpassword35345$!"
+        )
+
+    def test_sql_injection_in_video_title(self):
+        malicious_input = r"Tutorial de LOL'; SHUTDOWN; --"
+
+        try:
+            video = Video.objects.create(
+                title=malicious_input,
+                description="Uma introdução ao deep learning.",
+                slug=slugify("Deep Learning"),
+                author=self.user,
+            )
+            self.fail(
+                "Video was created despite SQL injection attempt, indicating a vulnerability."
+            )
+        except IntegrityError:
+            pass
+        except Exception as e:
+            self.assertNotIn("SHUTDOWN", str(e), "SQL Injection vulnerability detected")
+
+        video_injected = Video.objects.get(title=malicious_input)
+        self.assertEqual(video_injected.title, malicious_input)
+
+    def tearDown(self):
+        Video.objects.all().delete()
+
